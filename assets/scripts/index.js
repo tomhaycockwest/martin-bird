7a7bc2646f5f68df26a9f8b66a9b1f0c93e59f16
Tom Haycock-West <tomhaycockwest@gmail.com>
2015-12-01 20:40:29 +0000

    set intro height to window height

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,5 +3,10 @@
 // Requires
 var $ = require( 'jquery' );
 
+var windowHeight = $(window).height();
 
-// $(".intro").backstretch("../header.jpg");
+// set intro to be window height and have full size bg image
+$('.intro').height(windowHeight);
+$(".intro").backstretch("/assets/img/header.jpg");
+
+
