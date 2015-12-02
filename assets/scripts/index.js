8a3f1e447ef49c84d81f90c89753fa63df732e29
Tom Haycock-West <tomhaycockwest@gmail.com>
2015-12-02 21:28:32 +0000

    build gallery section
    add in isotope
    add bg colour to header on scroll

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,7 +6,50 @@ var $ = require( 'jquery' );
 var windowHeight = $(window).height();
 
 // set intro to be window height and have full size bg image
-$('.intro').height(windowHeight);
-$(".intro").backstretch("/assets/img/header.jpg");
+$('.section--intro').height(windowHeight);
+$(".section--intro").backstretch("/assets/img/header.jpg");
+
+
+// add bg colour to header if user scrolls
+$(document).scroll(function() {
+  if ($(document).scrollTop() === 0) {
+  	$('.header').removeClass('not-top');
+  } else {
+  	$('.header').addClass('not-top');
+  }
+});
+
+// GALLERY
+var gallery = $('.gallery');
+
+gallery.isotope({ 
+	layoutMode: 'cellsByRow',	
+	itemSelector: '.gallery-item',
+    isOriginLeft: false,
+	cellsByRow: {
+      columnWidth: 260,
+      rowHeight: 250,
+      percentPosition: true
+    },
+    getSortData: {
+		name: '.name',
+		symbol: '.symbol',
+		number: '.number parseInt',
+		category: '[data-category]',
+		weight: function( itemElem ) {
+		var weight = $( itemElem ).find('.weight').text();
+		return parseFloat( weight.replace( /[\(\)]/g, '') );
+		}
+	}
+});
+
+
+// bind filter button click
+$('.gallery-nav-item').click( function(e) {
+	e.preventDefault();
+	var filterValue = $( this ).attr('data-filter');
+	gallery.isotope({ filter: filterValue });
+});
+
 
 
